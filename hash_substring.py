238aa129b621b2d92383cc550d027589986b3292
Ričards <Ricards.helmanis@edu.rtu.lv>
2023-04-16 14:47:14 +0300

    Implemented inputs

--- hash_substring.py
+++ hash_substring.py
@@ -3,8 +3,23 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    input_method = input()
+
+    if input_method.__contains__("I"):
+        pattern = input()
+        text = input()
     
-    
+    elif input_method.__contains__("F"):
+        try:
+            with open("tests/06","r") as f:
+                pattern = f.readline()
+                text = f.readline()
+        except FileNotFoundError:
+            return
+
+    else:
+        print("Input error")
+        return
     # after input type choice
     # read two lines 
     # first line is pattern 
@@ -13,7 +28,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (pattern.rstrip(), text.rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -23,7 +38,7 @@ def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
 
     # and return an iterable variable
-    return [0]
+    return (pattern,text)
 
 
 # this part launches the functions
